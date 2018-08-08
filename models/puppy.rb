77e301dc21a6fdb3b2a1514304ffb554d1e0eb19
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-08 22:18:16 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,11 @@
-class Puppy 
-  attr_accessor :name, :breed, :age 
+class Puppy
+  attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
     @name = name
-    @breed = breed 
+    @breed = breed
     @age = age
+  end 
 
 
-end 
+end
